b77f81d247adce8bb94c145a8a8a25ec3d611ad5
Jesse de Poot <jjar.depoot@student.avans.nl>
2025-12-08 13:34:38 +0100

    Login Multiword error fixed

--- Front End Jesse/smartplanter-jesse/src/pages/Login.vue
+++ Front End Jesse/smartplanter-jesse/src/pages/Login.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <button @click="login">Login via Keycloak</button>
+  <div class="page">
+    <div class="container">
+      <h1>Login</h1>
+      <button @click="login">Login via Keycloak</button>
+    </div>
   </div>
 </template>
 
@@ -9,6 +11,7 @@
 import { keycloak } from '../keycloak'
 
 export default {
+  name: 'LoginPage', 
   setup() {
     const login = () => keycloak.login()
     return { login }
@@ -16,71 +19,68 @@ export default {
 }
 </script>
 
-
 <style>
+* {
+    margin: 0;
+    padding: 0;
+}
 
-    * {
-        margin: 0;
-        padding: 0;
-    }
+.page {
+    background: var(--bg);
+    height: 100vh;
+    width: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow-y: hidden;
+}
 
-    .page {
-        background: var(--bg);
-        height: 100vh;
-        width: auto;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        overflow-y: hidden;
-    }
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 25rem;
+    height: 30rem;
+    backdrop-filter: blur(2px);
+    background: var(--light);
+    padding: 20px;
+    border-radius: 3rem;
+}
 
-    .container {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        text-align: center;
-        width: 25rem;
-        height: 30rem;
-        backdrop-filter: blur(2px);
-        background: var(--light);
-        padding: 20px;
-        border-radius: 3rem;
-    }
+.container h1 {
+    color: var(--text);
+    font-size: 3rem;
+    user-select: none;
+}
 
-    .container h1 {
-        color: var(--text);
-        font-size: 3rem;
-        user-select: none;
-    }
+.container button {
+    display: flex;
+    text-decoration: none;
+    width: 15rem;
+    background: var(--primary);
+    font-size: 2rem; /* aangepast van 4rem, paste beter in button */
+    color: var(--primary-dark);
+    border: 2px solid var(--primary-dark);
+    border-radius: 50px;
+    justify-content: center;    
+    margin: 2rem;
+    font-weight: 400;
+    user-select: none;
+    cursor: pointer;
+    transition: all 200ms ease;
+}
 
-    .container button {
-        display: flex;
-        text-decoration: none;
-        width: 15rem;
-        background: var(--primary) ;
-        font-size: 4rem;
-        color: var(--primary-dark);
-        border: 2px solid var(--primary-dark);
-        border-radius: 50px;
-        justify-content: center;    
-        margin: 2rem;
-        font-weight: 400;
-        user-select: none;
-        cursor: pointer;
-        transition: all 200ms ease;
-    }
+.container button:hover {
+    background: var(--primary-dark);
+    color: var(--primary);
+}
 
-    .container button:hover {
-        background: var(--primary-dark);
-        color: var(--primary);
-    }
-
-    .container i {
-        font-size: 10rem;
-        height: auto;
-        user-select: none;
-        color: var(--primary);
-    }
-
+.container i {
+    font-size: 10rem;
+    height: auto;
+    user-select: none;
+    color: var(--primary);
+}
 </style>
