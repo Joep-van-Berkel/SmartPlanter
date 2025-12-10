8219d8a8e90b9976e8732a7a5aaaa069311c6af4
Jesse de Poot <jjar.depoot@student.avans.nl>
2025-12-10 12:14:33 +0100

    Keycloak version package update

--- Front End Jesse/smartplanter-jesse/src/main.js
+++ Front End Jesse/smartplanter-jesse/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import Keycloak from 'keycloak-js';
+import Keycloak from 'keycloak-js'
 
 // --- STAP 1: Keycloak Configuratie ---
 const initOptions = {
