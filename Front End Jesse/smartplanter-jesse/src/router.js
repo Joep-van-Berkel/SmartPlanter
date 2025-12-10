54b6bb2e8658ce0bfde2be00b77990aa3190ff37
Jesse de Poot <jjar.depoot@student.avans.nl>
2025-12-10 13:14:04 +0100

    Router fixed

---
+++ Front End Jesse/smartplanter-jesse/src/router.js
@@ -0,0 +1,47 @@
+// router.js
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardPage from './pages/DashboardPage.vue'
+import SettingsPage from './pages/SettingsPage.vue'
+import DataPage from './pages/DataPage.vue'
+import NotificationsPage from './pages/NotificationsPage.vue'
+
+let keycloakInstance = null
+
+// Functie om Keycloak binnen router te kunnen gebruiken
+export function setKeycloak(keycloak) {
+  keycloakInstance = keycloak
+}
+
+const routes = [
+  { path: '/', component: DashboardPage },
+  { path: '/notifications', component: NotificationsPage },
+  { path: '/data', component: DataPage },
+  { path: '/settings', component: SettingsPage },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+// NAVIGATION GUARD
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresRole) {
+    if (!keycloakInstance) {
+      console.error("⚠️ Keycloak is not set yet in router.")
+      return next('/')
+    }
+
+    const hasRole = keycloakInstance.hasRealmRole(to.meta.requiresRole)
+
+    if (hasRole) next()
+    else {
+      alert('⛔ Geen toegang! Je hebt geen admin rechten.')
+      next('/')
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
