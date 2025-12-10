c21917224441922d37952ec6cd610cb9d0bb7242
Immepiels07 <ima.piels@student.avans.nl>
2025-12-10 10:10:12 +0100

    router test 5

--- Front End Imme/smartplanter-imme/src/main.js
+++ Front End Imme/smartplanter-imme/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './Router'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
